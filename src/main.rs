476c1be8ec27884c10c2b18ae6b6aa33e37b9301
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-03-05 22:41:59 +0530

    setup basic browser code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod types;
+
+use headless_chrome::{Browser, LaunchOptions};
+
 fn main() {
+
+    const url: &str = "https://lining.studio/sales/guest/form/";
+
+    let browser = Browser::new(
+        LaunchOptions::default_builder()
+            .headless(false)
+            .build()
+            .unwrap(),
+    ).unwrap();
+
+    let tab = browser.wait_for_initial_tab().unwrap();
+
     println!("Hello, world!");
 }
